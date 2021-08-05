d7337f93f1e619c23e038f9d46c88ea62822d5b5
Krista Marbury <laetaiustus@gmail.com>
2021-08-05 17:56:30 -0500

    updated server page

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://droidplease8:droidplease8@cluster0.5r0q7.mongodb.net/workoutdb?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 app.listen(PORT, () => {
